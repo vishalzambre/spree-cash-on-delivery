834df2ded429f6847190b6ed5f0b326f86d938a0
Vishal Zambre <vishal.zambre@weboniselab.com>
2015-09-17 21:36:40 +0530

    fea changes

--- app/models/spree/cash_on_delivery/payment_method.rb
+++ app/models/spree/cash_on_delivery/payment_method.rb
@@ -1,19 +1,23 @@
 module Spree
   class CashOnDelivery::PaymentMethod < Spree::PaymentMethod
     def payment_profiles_supported?
-      true # we want to show the confirm step.
+      false # we want to show the confirm step.
     end
 
-    def provider_class
-      self.class
-    end
-  
     def payment_source_class
       Spree::CashOnDelivery::Transaction
     end
-  
+
+    def source_required?
+      false
+    end
+
     def method_type
       'cash_on_delivery'
+    end
+
+    def authorization
+      self
     end
   end
 end
--- app/models/spree/cash_on_delivery/transaction.rb
+++ app/models/spree/cash_on_delivery/transaction.rb
@@ -13,17 +13,18 @@ module Spree
         event :receive_cash do
           transition 'goods_sent' => 'cash_received'
         end
-  
+
         event :void_txn do
           transition 'goods_sent' => 'canceled'
         end
       end
 
       def post_create(payment)
-        payment.order.adjustments.create(:amount => Spree::Config[:cash_on_delivery_charge],
-                                 :source => payment,
-                                 :originator => payment,
-                                 :label => I18n.t(:shipping_and_handling))
+        payment.order.adjustments.create(
+          amount: Spree::Config[:cash_on_delivery_charge],
+          source: payment,
+          label: I18n.t(:shipping_and_handling)
+        )
       end
 
       def update_adjustment(adjustment, src)
@@ -33,33 +34,33 @@ module Spree
       def actions
         %w{capture void cash_received}
       end
-   
+
       def can_capture?(payment)
         ['checkout', 'pending'].include?(payment.state)
       end
-    
+
       def can_void?(payment)
         payment.state != 'void' and ['goods_sent', 'initial'].include?(payment.source.state)
       end
-   
+
       def can_cash_received?(payment)
         payment.state == 'completed' and payment.source.state == 'goods_sent'
       end
-   
+
       def capture(payment)
         payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
         payment.complete
         payment.source.send_goods
         true
       end
-   
+
       def void(payment)
         payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
         payment.void
         payment.source.void_txn
         true
       end
-   
+
       def cash_received(payment)
         payment.source.receive_cash
         true
